32c1de57ccf63acf793d57577c713889d44a084b
muhdwaseem <waseemvann@gmail.com>
2021-06-23 23:44:42 +0530

    complete

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log("hello script is working")
+var btntranslate = document.querySelector("#bt-translate");
+var txtarea = document.querySelector("#txtarea");
+var outputdiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function gettranslationalurl(text){
+    return serverURL + "?"+  "text=" + text
+}
+function errorhandler(error){
+    console.log("error occuerd",error)
+    alert("somthing wroung with server")
+}
+function clickEventHandler(){
+ //   outputdiv.innerText=  "translated"   +    txtarea.value;
+var inputText = txtarea.Value;
+
+ fetch(gettranslationalurl(inputText))
+ .then(Response => Response.json())
+ .then(json => {
+     var translatedText = json.content.translated;
+     outputdiv.innerText = translatedText;
+ })
+.catch(errorhandler)
+   
+};
+btntranslate.addEventListener("click",clickEventHandler)
+
+
+
+
+
