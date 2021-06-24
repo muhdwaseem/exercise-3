83d0d83f50f7cf7eac28aabcf9b8913c0c9135ef
muhdwaseem <waseemvann@gmail.com>
2021-06-24 12:24:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,28 +2,32 @@ var btntranslate = document.querySelector("#bt-translate");
 var txtarea = document.querySelector("#txtarea");
 var outputdiv = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL ="	https://api.funtranslations.com/translate/minion.json"
+
 
 function gettranslationalurl(text){
     return serverURL + "?"+  "text=" + text
 }
 function errorhandler(error){
-    console.log("error occuerd",error)
-    alert("somthing wroung with server")
+    console.log("error occuerd",error);
+    alert("somthing wrong with server try again after some tiime")
 }
+
 function clickEventHandler(){
- //   outputdiv.innerText=  "translated"   +    txtarea.value;
+ 
 var inputText = txtarea.Value;
 
  fetch(gettranslationalurl(inputText))
  .then(Response => Response.json())
- .then(json => {
-     var translatedText = json.content.translated;
-     outputdiv.innerText = translatedText;
- })
-.catch(errorhandler)
+ .then(json =>  {
+     var translatedtext = json.contents.translated;
+     outputdiv.innerText = translatedtext;
+ }
+ )
+ .catch(errorhandler)
+
    
-};
+}
 btntranslate.addEventListener("click",clickEventHandler)
 
 
